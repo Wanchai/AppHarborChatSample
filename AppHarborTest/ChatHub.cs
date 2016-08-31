2832b6fe08c9265ca80db11f4c554fc0cbe29df7
wanchai <thomas.malicet@gmail.com>
2016-08-31 21:03:19 +0200

    a

---
+++ AppHarborTest/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace AppHarborTest
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+        public string[] GetClients()
+        {
+            return
+        }
+    }
+}
