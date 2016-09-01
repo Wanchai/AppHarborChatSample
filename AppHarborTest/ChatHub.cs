94c867f71161a10228dfd07b24305f746e924a33
wanchai <thomas.malicet@gmail.com>
2016-09-01 14:56:41 +0200

    Adding Users List

--- AppHarborTest/ChatHub.cs
+++ AppHarborTest/ChatHub.cs
@@ -1,21 +1,49 @@
 ﻿using System;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using System.Collections.Generic;
 
 namespace AppHarborTest
 {
     public class ChatHub : Hub
     {
+
+        static List<string> users = new List<string>();
+
         public void Send(string name, string message)
         {
             // Call the broadcastMessage method to update clients.
             Clients.All.broadcastMessage(name, message);
         }
-        /*
-        public string[] GetClients()
+
+        public void AddUser(string name)
         {
-            return
+            users.Add(name);
+            Clients.All.refreshUser(users);
         }
-        */
+        public void DeleteUser(string name)
+        {
+            users.Remove(name);
+        }
+
+        public string[] GetUsers()
+        {
+            
+            return users.ToArray();
+        }
+
+        /*
+    public string[] GetClients()
+    {
+        Clients.All
+        return
+    }
+    */
+
+    }
+
+    class Users
+    {
+        public string name;
     }
 }
